f299fdb51241c46b89e415d6e2f8ede27f7adc61
masterbogdan <maiskiychavk@gmail.com>
2020-08-06 21:35:36 +0300

    Updates

---
+++ precache-manifest.4b1e6610da97db99480be016de3ed45a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9e355bcacfe603b107f454743c5c60",
+    "url": "/react-friends-app/index.html"
+  },
+  {
+    "revision": "ec5112559a6f84e5e3a0",
+    "url": "/react-friends-app/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "c0f74dda66ba0685c320",
+    "url": "/react-friends-app/static/css/main.d17893f6.chunk.css"
+  },
+  {
+    "revision": "ec5112559a6f84e5e3a0",
+    "url": "/react-friends-app/static/js/2.19a7297c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-friends-app/static/js/2.19a7297c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0f74dda66ba0685c320",
+    "url": "/react-friends-app/static/js/main.adf069fa.chunk.js"
+  },
+  {
+    "revision": "1caf403ebb8d5009c6b8",
+    "url": "/react-friends-app/static/js/runtime-main.00cb4478.js"
+  }
+]);
